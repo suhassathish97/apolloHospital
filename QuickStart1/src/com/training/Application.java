a4271491a73cc2019952ce04e5123c9d10784801
Suhas S <suhassathish97@gmail.com>
2019-11-06 12:53:09 +0530

    Added ForEach loop - requirement 1.0

--- QuickStart1/src/com/training/Application.java
+++ QuickStart1/src/com/training/Application.java
@@ -7,5 +7,9 @@ public class Application {
 	public static void main(String args[]) {
 		List<String> nameList = Arrays.asList("Ramesh","Suresh","Magesh");
 		nameList.forEach(System.out::println);
+		
+		for(String eachItem:nameList) {
+			System.out.println(eachItem);
+		}
 	}
 }
